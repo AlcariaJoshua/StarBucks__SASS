de273ec47c0c52e64e1faee6e76ff9aaa32fdc7b
AlcariaJoshua <alcariajoshua13@gmail.com>
2023-03-13 16:00:43 +0800

    commit

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,43 @@
+
+const toggleMenu = document.querySelector(".burger__btn");
+const headerBot = document.querySelector(".toggle");
+
+toggleMenu.addEventListener("click", () => {
+  toggleMenu.classList.toggle("open");
+  headerBot.classList.toggle("open");
+});
+
 const kamote = document.querySelectorAll('.links__item h4');
 kamote.forEach((kamoteq) => {
     kamoteq.addEventListener('click', () => {
         kamoteq.nextElementSibling.classList.toggle('open');
         kamoteq.querySelector("i").classList.toggle("open");
+       
     })
 })
+
+const starnav = document.querySelectorAll(".star__nav__link");
+const starContent = document.querySelectorAll(".coffee-tab__content");
+
+starnav.forEach((star) => {
+  star.addEventListener('click', () =>{
+    removeActiveStar();
+    star.classList.add('.active');
+    const activeContent= document.querySelector(`#${star.id}-content`);
+    removeActiveContent();
+    activeContent.classList.add("active");
+  })
+});
+
+function removeActiveStar() {
+  starnav.forEach((star) => {
+    star.classList.remove('active');
+  })
+}
+function removeActiveStar() {
+  starContent.forEach((star) => {
+    star.classList.remove('active');
+  })
+}
+
 
